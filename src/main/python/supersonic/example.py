1dbfdcb2218bf631203b9d04cce49874ee3fc517
Fontany--Legall Brandon <brandon.fontany--legall@etu.unice.fr>
2020-01-21 17:22:03 +0100

    supersonic detector

---
+++ src/main/python/supersonic/example.py
@@ -0,0 +1,15 @@
+# GrovePi + Grove Ultrasonic Ranger from grovepi import *
+# Connect the Grove Ultrasonic Ranger to digital port D4 # SIG,NC,VCC,GND
+
+from grovepi import *
+# Connect the Grove Ultrasonic Ranger to digital port D4 # SIG,NC,VCC,GND
+ultrasonic_ranger = 3
+
+while True:
+    try:
+        # Read distance value from Ultrasonic
+        print ultrasonicRead(ultrasonic_ranger)
+    except TypeError:
+        print "Error"
+    except IOError:
+        print "Error"
