bfd7f36c9070198a80975aa214ef0adf3c011bbe
FontanyLegall-Brandon <brandon@fontany-legall.xyz>
2020-01-26 15:58:26 +0100

    Update doc

--- mqtt_based/smartbowl/main.py
+++ mqtt_based/smartbowl/main.py
@@ -5,6 +5,9 @@ import schedule
 import json
 from bricks.smartbowl import *
 
+"""
+Entry point of smartbowl
+"""
 if __name__ == "__main__":
     Smartbowl(mqtt_cloud_url="mqtt://ddlpfjur:xYB7g87hyXyF@hairdresser.cloudmqtt.com:18320",
                           mqtt_rasp_url="mqtt://localhost:1883")
