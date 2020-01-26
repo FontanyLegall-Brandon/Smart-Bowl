11e65fc06b4e61d1fb63c99d93b820f51795bc2e
AymericValdenaire <aymericvaldenaire@hotmail.fr>
2020-01-26 16:00:46 +0100

    cgi-bin header added

--- mqtt_based/smartbowl/main.py
+++ mqtt_based/smartbowl/main.py
@@ -1,9 +1,13 @@
+#! /bin/python3
+
 import paho.mqtt.client as mqtt
 import os
 from urllib.parse import urlparse
 import schedule
 import json
 from bricks.smartbowl import *
+
+
 
 """
 Entry point of smartbowl
